3679829cd2de26bdfa0157c4f0d5f1ded7440b53
manubeta12 <manubeta2006@gmail.com>
2024-02-22 12:14:38 -0500

    INICIO

--- Empleado/Empleado.py
+++ Empleado/Empleado.py
@@ -1,7 +1,31 @@
 class empleado: 
     nombre= ""
     apellido= ""
-    genero= "masculino"
+    sexo= "masculino"
     #1= Masculino y 2= Femenino
+
     salario= 0
-#Aqui va el codigo del empleado
+    # Metodos
+    def CambiarSalario(self,nuevoSalario):
+        #Aqui va el codigo del metodo
+        return 0
+    
+    def CambiarEmpleado(self,nNombre,nApellido,nSexo,nSalario):
+        #Aqui va el codigo el nuevo empleado
+        return None 
+    
+    def ConsultarSalario(self):
+        #Aqui va el codigo del metodo
+        return self.salario 
+    
+    def ConsultarNombre(self):
+        #Aqui va el codigo del metodo
+        return self.nombre 
+    
+    def ConsultarApellido(self):
+        #Aqui va el codigo del metodo
+        return self.apellido
+    
+    def ConsultarNombreCompleto(self):
+        #Aqui va el codigo del metodo 
+        return self.NombreaCompleto 
