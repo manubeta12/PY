d8a58fae1a2279475f154a316504b09307af75ce
manubeta12 <160885871+manubeta12@users.noreply.github.com>
2024-02-26 23:53:27 +0000

    nuevo

--- simuladorBancario/CuentaAhorro.py
+++ simuladorBancario/CuentaAhorro.py
@@ -2,3 +2,16 @@ class CuentaAhorro:
     saldo=""
     InteresMensual=""
 #aqui va el codigo de CuentaAhorro
+def ConsignarValor (self):
+    nsaldo= self.saldo + 100000
+    self.saldo= nsaldo
+    return" El nuevo saldo mas el valor consignado es" + self.saldo 
+def RetirarValor(self):
+    nSaldo= self.saldo - 100000
+    self.saldo = nSaldo
+    return" El nuevo saldo menos el valor retirado es" + self.saldo 
+def InteresMensual (self):
+    nSaldo= self.saldo * 0.6
+    self.saldo = nSaldo
+    return" El nuevo saldo mas el inetres mensual es" + self.saldo
+
