622fba4938aeeab316755a40483eb82af6e0a8a9
manubeta12 <160885871+manubeta12@users.noreply.github.com>
2024-03-05 18:46:37 +0000

    inicio

---
+++ Empleado/Empleado1/Empleado.py
@@ -0,0 +1,24 @@
+from fecha import Fecha
+from OtroEmpleado import OtroEmpleado
+
+
+
+class Empleado:
+    # codigo para determinar la clase del empleado 
+
+    # Atributo 
+
+    nombre = "Nombre del empleado"
+    apellido = "Apellido del empleado"
+    hijo = "Numero de hijos del empleado"
+
+    # 1 = Masculino y 2 = Femenino
+
+sexo= 1
+salario= 2000000
+
+# Asociacion
+
+otroEmpleado = OtroEmpleado()
+fechaNacimiento = Fecha()
+fechaIngreso = Fecha ()
